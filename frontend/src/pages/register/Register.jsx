dd2f8bf3605220a9f73736dea20d1d061b69030c
Afonso <afonsoserrao07@gmail.com>
2023-12-10 14:09:49 +0000

    falta acabar o register

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
-import {Link} from'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import './register.css'
 
 export default function Register() {
   return (
-    <div>
-      <div className="left">
-        
-      </div>
-      <div className="right">
-      <h2>Welcome!</h2>
-        <div className="r">
-          <span>Already have an account? <Link to='/login'>Login</Link></span>
-        </div>
+    <div className='register'>
+      <div className='box1'>
+          <div className='head1'>
+            <h1>Register</h1>
+          </div>
+          <form className='form1'>
+            <div className='inputs1'>
+              <label>Name:</label>
+              <input type='text' />
+            </div>
+            <div className='inputs1'>
+              <label>Surname:</label>
+              <input type='text' />
+            </div>
+            <div className='inputs1'>
+              <label>Username:</label>
+              <input type='text' />
+            </div>
+            <div className='inputs1'>
+              <label>Email:</label>
+              <input type='text' />
+            </div>
+            <div className='inputs1'>
+              <label>Password:</label>
+              <input type='password1' />
+            </div>
+            <input type='button' value='Login' className='btn1' />
+          </form>
+          <div className='menos1'>
+            <span>
+              Already have an account? <Link to='/register'>Register</Link>
+            </span>
+          </div>
       </div>
     </div>
-  )
+  );
 }
