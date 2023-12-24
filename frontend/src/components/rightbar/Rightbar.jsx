fa2657cb6682d758288b98b9cd6140560e1de3ae
Afonso <afonsoserrao07@gmail.com>
2023-12-24 18:14:06 +0000

    a

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./rightbar.css";
+import Guy from "../../assets/imgs/guy.jpg";
 
 export default function Rightbar() {
   return (
-    <div>Rightbar</div>
-  )
+    <div>
+      <div className='caixa'>
+        <div className='pessoas'>
+          <div className='divPessoa'>
+            <img src={Guy} className='pessoa' />
+          </div>
+          <div className='divPessoa'>
+            <img src={Guy} className='pessoa' />
+          </div>
+          <div className='divPessoa'>
+            <img src={Guy} className='pessoa' />
+          </div>
+          <div className='divPessoa'>
+            <img src={Guy} className='pessoa' />
+          </div>
+          <div className='divPessoa'>
+            <img src={Guy} className='pessoa' />
+          </div>
+          <div className='divPessoa'>
+            <img src={Guy} className='pessoa' />
+          </div>
+        </div>
+        <div className='conversa'>
+          <div className='nome'></div>
+          <div className='chat'></div>
+          <div className='enviar'></div>
+        </div>
+      </div>
+    </div>
+  );
 }
