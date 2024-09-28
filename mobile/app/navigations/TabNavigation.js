0ae91fa5214053db52ff9d845eca50b50a246214
Afonso <afonsoserrao07@gmail.com>
2024-09-28 11:28:47 +0100

    nao tenho pachorra para isto

--- mobile/app/navigations/TabNavigation.js
+++ mobile/app/navigations/TabNavigation.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FontAwesome } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Entypo from '@expo/vector-icons/Entypo';
 import { UserContext } from '../context/userContext';
 
 export default function TabNavigation() {
@@ -30,7 +31,29 @@ export default function TabNavigation() {
         )
       }}/>
           </>
-        ) : ''
+        ) : <>
+        <Tab.Screen name="Home" component={''} options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({color, size})=>(
+          <Entypo name="home" size={size} color={color} />
+        )
+      }}/><Tab.Screen name="Friends" component={''} options={{
+        tabBarLabel: 'Friends',
+        tabBarIcon: ({color, size})=>(
+          <FontAwesome name="group" size={size} color={color} />
+        )
+      }}/><Tab.Screen name="Messages" component={''} options={{
+        tabBarLabel: 'Messages',
+        tabBarIcon: ({color, size})=>(
+          <Entypo name="message" size={size} color={color} />
+        )
+      }}/><Tab.Screen name="Profile" component={''} options={{
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({color, size})=>(
+          <FontAwesome name="user" size={size} color={color} />
+        )
+      }}/>
+        </>
       }
       
     </Tab.Navigator>
